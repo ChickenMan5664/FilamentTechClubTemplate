a45c58e2d2b4872c127221fe806c17f653017460
ChickenMan5664 <133166618+ChickenMan5664@users.noreply.github.com>
2023-05-12 17:53:46 -0500

    more JS

--- scripy/script.js
+++ scripy/script.js
@@ -1,4 +1,23 @@
 
 function changeColor() {
-    document.getElementByld("keuntuckyfried").style.color = "red";
+    document.getElementByld("keuntuckyfried").style.color;
+   var newColor = selectNewColor(currentColor);
+   document.getElementByld("helloworld").style.color = newColor;
+}
+
+function selectNewColor(currentcolor) {
+    var newColor = "red";
+
+    if (currentColor === "") {
+        newColor = "red";
+    } else if (currentColor === "red") {
+        newColor = "purple";
+    } else if (currentColor === "purple") {
+        newColor = "green";
+    } else if (currentColor === "green") {
+        newColor = "";
+    }
+
+    return newColor;
+
 }
